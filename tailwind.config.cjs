f2404bfb8dccfaddf651ec4880dc97acad3f837e
je-boska <joneirikboska@gmail.com>
2023-01-04 21:07:32 +0100

    Change fonts to Roboto Mono and Helvetica, adjust logos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      sans: ['Mukta', 'Helvetica', 'sans-serif'],
+      sans: ['Helvetica', 'sans-serif'],
+      mono: ['Roboto Mono', 'sans-serif'],
     },
     extend: {},
   },
